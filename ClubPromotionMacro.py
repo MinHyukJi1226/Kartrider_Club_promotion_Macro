09b3d52115b88dcc5fcf30663843a3422e868daf
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-05-28 18:11:50 +0900

    [Add] Go to the Loginpage
    로그인 버튼을 눌렀을 때 로그인 페이지로 넘어가는 기능 구현

--- ClubPromotionMacro.py
+++ ClubPromotionMacro.py
@@ -5,6 +5,9 @@ driver = webdriver.Chrome()
 url = 'https://kart.nexon.com/Kart/Guild/Story/List.aspx'
 driver.get(url)
 
+xpath = "//img[@src='https://ssl.nx.com/s2/game/kart/v2/common/btn_gc_login.jpg']"
+
+driver.find_element_by_xpath(xpath).click()
 
 # 코드에 아이디와 비밀번호를 저장하지 않기위해 따로 입력 받음
 print('아이디를 입력해주세요.')
