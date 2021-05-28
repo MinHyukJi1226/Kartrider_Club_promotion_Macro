b6361baee124954e08d42500336a69a86b5793f3
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-05-28 17:41:01 +0900

    ADD Auto Login
    기능만 만들어 놓음

--- ClubPromotionMacro.py
+++ ClubPromotionMacro.py
@@ -1,5 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 url = 'https://kart.nexon.com/Kart/Guild/Story/List.aspx'
 driver.get(url)
+
+
+# 코드에 아이디와 비밀번호를 저장하지 않기위해 따로 입력 받음
+print('아이디를 입력해주세요.')
+userId = input() 
+
+print('비밀번호를 입력해주세요.')
+userPw = input()
+
+driver.find_element_by_id('txtNexonID').send_keys(userId)
+driver.find_element_by_id('txtPWD').send_keys(userId)
+driver.find_element_by_class_name('button01').click()
