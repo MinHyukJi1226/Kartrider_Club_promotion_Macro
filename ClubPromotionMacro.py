dc6a4cdf7b8c621537f6d91ef7492374c84391e6
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-06-01 19:35:38 +0900

    Create GUI
    기본 GUI 작성

--- ClubPromotionMacro.py
+++ ClubPromotionMacro.py
@@ -1,55 +1,87 @@
+from typing import Collection
 from selenium import webdriver
 from selenium.webdriver.common import keys
 from selenium.webdriver.common.keys import Keys
+from tkinter import *
 import time
 
-driver = webdriver.Chrome()
-url = 'https://kart.nexon.com/Kart/Guild/Story/List.aspx'
-driver.get(url)
+from selenium.webdriver.remote import command
+from urllib3.packages.six import u
 
-xpath = "//img[@src='https://ssl.nx.com/s2/game/kart/v2/common/btn_gc_login.jpg']"
+def runMecro():
 
-driver.find_element_by_xpath(xpath).click()
+    userId = idTxt.get()
+    userPw = pwTxt.get()
+    postTitle = titleTxt.get()
 
-# 코드에 아이디와 비밀번호를 저장하지 않기위해 따로 입력 받음
-print('아이디를 입력해주세요.')
-userId = input() 
+    driver = webdriver.Chrome()
+    url = 'https://kart.nexon.com/Kart/Guild/Story/List.aspx'
+    driver.get(url)
 
-print('비밀번호를 입력해주세요.')
-userPw = input()
+    xpath = "//img[@src='https://ssl.nx.com/s2/game/kart/v2/common/btn_gc_login.jpg']"
 
-driver.find_element_by_id('txtNexonID').send_keys(userId)
-driver.find_element_by_id('txtPWD').send_keys(userPw)
-driver.find_element_by_class_name('button01').click()
+    driver.find_element_by_xpath(xpath).click()
 
-time.sleep(0.8) #페이지가 제대로 열릴때 까지 기다림
+    driver.find_element_by_id('txtNexonID').send_keys(userId)
+    driver.find_element_by_id('txtPWD').send_keys(userPw)
+    driver.find_element_by_class_name('button01').click()
 
-xpath = "//img[@src='https://ssl.nx.com/s2/game/kart/v2/community/btn_write.gif']"
+    time.sleep(0.8) #페이지가 제대로 열릴때 까지 기다림
 
-driver.find_element_by_xpath(xpath).click()
+    xpath = "//img[@src='https://ssl.nx.com/s2/game/kart/v2/community/btn_write.gif']"
 
-time.sleep(0.5)
+    driver.find_element_by_xpath(xpath).click()
 
-title = "(매우빠름) Lv.2 Realize 길드원 모집"
+    time.sleep(0.5)
 
-driver.find_element_by_name('ctl00$ContentPlaceHolder1$m_txtTitle').send_keys(title)
+    driver.find_element_by_name('ctl00$ContentPlaceHolder1$m_txtTitle').send_keys(postTitle)
 
-time.sleep(0.5)
+    time.sleep(0.5)
 
-driver.find_element_by_class_name('uploadImage').click()
+    driver.find_element_by_class_name('uploadImage').click()
 
-driver.switch_to.window(driver.window_handles[-1]) # 새로운 창으로 변경
+    driver.switch_to.window(driver.window_handles[-1]) # 새로운 창으로 변경
 
-time.sleep(0.5)
+    time.sleep(0.5)
 
-xpath = "//input[@type = 'file']"
+    xpath = "//input[@type = 'file']"
 
-driver.find_element_by_xpath(xpath).send_keys("C:/Users/Minhyuk/Downloads/img.png")
+    driver.find_element_by_xpath(xpath).send_keys("C:/Users/Minhyuk/Downloads/img.png")
 
-driver.find_element_by_id('btnUpload').click()
+    driver.find_element_by_id('btnUpload').click()
 
-driver.switch_to.window(driver.window_handles[0]) # 원래 창으로 변경
+    driver.switch_to.window(driver.window_handles[0]) # 원래 창으로 변경
 
-time.sleep(1)
+    time.sleep(1)
 
-driver.find_element_by_id('ContentPlaceHolder1_btnWrite').click()
+    driver.find_element_by_id('ContentPlaceHolder1_btnWrite').click()
+
+root = Tk()
+root.title("카트라이더 클럽 홍보 프로그램")
+
+title = Label(root, text="카트라이더 클럽 홍보 매크로")
+
+idLable = Label(root, text="아이디 :")
+pwLable = Label(root, text="비밀번호 :" )
+titleLable = Label(root, text="게시물 제목 : ")
+
+idTxt = Entry(root, width=30)
+pwTxt = Entry(root, width=30)
+titleTxt = Entry(root, width=30)
+
+pwTxt.config(show='*')
+
+photo = Button(root, text="사진 업로드")
+run = Button(root, text="실행", command = lambda : runMecro())
+
+title.grid(row=0, column=1)
+idLable.grid(row=1, column=0)
+idTxt.grid(row=1, column=1)
+pwLable.grid(row=2, column=0)
+pwTxt.grid(row=2, column=1)
+titleLable.grid(row=3, column=0)
+titleTxt.grid(row=3, column=1)
+photo.grid(row=4, column=0)
+run.grid(row=4, column=1)
+
+root.mainloop()
